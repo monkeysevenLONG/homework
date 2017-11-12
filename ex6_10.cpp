50ca5dbb2e826a3444254ec3e6229298994df43d
monkeysevenLONG <31696850+monkeysevenLONG@users.noreply.github.com>
2017-11-12 23:29:40 +0800

    Add files via upload

---
+++ ex6_10.cpp
@@ -0,0 +1,17 @@
+#include <iostream.h>
+float fun (double h)
+{
+long t;
+t=(h*1000+5)/10;
+return (float) t/100;
+}
+main()
+{
+float a;
+cout<<"Enter a: ";
+cin>>a;
+cout<<"The original data is : ";
+cout<<a<<endl;
+cout<<"The result:"<<fun(a)<<endl;
+return 0;
+}
