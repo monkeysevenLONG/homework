8d1590f3c04feb29fd455e01a6d50998cb4687dc
monkeysevenLONG <31696850+monkeysevenLONG@users.noreply.github.com>
2023-11-07 15:30:46 +0800

    Add files via upload

---
+++ enum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+enum color { red, green, blue };
+char name[][10] ={
+	"red",
+		"green",
+		"blue"
+};
+int main()
+{
+	enum color clr;
+	for (clr = red; clr <= blue; clr++)
+	{
+		printf("%d % s\n",clr, name[clr]);
+	}
+
+	return 0;
+}
